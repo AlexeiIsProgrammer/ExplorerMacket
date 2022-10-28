6ab87b020ecc588a6daa1e072c7478a6bd132cee
Alexei <avenge.now.r@mail.ru>
2022-10-28 23:08:09 +0300

    add: create header with bootstrap

---
+++ src/index.js
@@ -0,0 +1,4 @@
+import './index.html';
+import './nullstyle.scss'
+import './styles.css';
+import '../node_modules/bootstrap/scss/bootstrap.scss';
--- webpack.config.js
+++ webpack.config.js
@@ -1,61 +1,77 @@
-const webpack = require('webpack');
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const mode = process.env.NODE_ENV || 'development';
+//npm init -y : инициализирует npm (запись в package.json)
+//npm install webpack -D (установка вебпака)
+//npm install webpack-cli webpack-dev-server -D (cli для управления вебпака, писать скрипты в package.json; server для использования своего сервера)
 
-const devMode = mode === 'development';
+const webpack = require('webpack'); //
+const path = require('path'); //правильные пути указывать
+const HtmlWebpackPlugin = require('html-webpack-plugin'); //Плагин для HTLM
+const MiniCssExtractPlugin = require('mini-css-extract-plugin'); //Плагин для CSS
+const { parseHTML } = require('jquery');
+const { postcss } = require('autoprefixer');
+const mode = process.env.NODE_ENV || 'development'; //указание среды разработки ()
 
-const target = devMode ? 'web' : 'browserslist';
-const devtool = devMode ? 'source-map' : undefined;
+const devMode = mode === 'development'; // Проверка мода на девелопмент
 
-const config = {
-    mode,
-    target,
-    devtool,
+const target = devMode ? 'web' : 'browserslist'; // Если development, то web настройки, если продакшн, то файл browserslistrc
+const devtool = devMode ? 'source-map' : undefined; // При дев моде, будет source-map для отслежиания ошибок 
+
+module.exports = {
+    mode, //Мод (дев или прод)
+    target, // ресурс (web или browserslistrc)
+    devtool, // отслеживание ошибок
     devServer: {
-        port: 3000,
-        open: true,
-        hot: true,
+        port: 3000, //Порт
+        open: true, //Открывать браузер
+        hot: true, //Обновление стилей (бывают сложности)
     },
-    entry: './src/index.js',
+    entry: ['@babel/polyfill', path.resolve(__dirname, 'src', 'index.js')], //npm install --save @babel/polyfill : для поддержки скриптов из браузеров
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js'
+        clean:true,
+        filename: 'index.js', //name - main по умолчанию, contenthash нужен для сборки разных имен, чтобы обновление было из разных файлов
     },
     module: {
         rules: [
-            {
-                test: /\.js$/,
-                use: 'babel-loader',
-                exclude: /node_modules/
+            { //npm install --save-dev html-loader
+                test: /\.html$/i,
+                loader: 'html-loader',
             },
-            {
-                test: /\.css$/,
+            {//npm install --save-dev @babel/core @babel/preset-env babel-loader
+                test: /\.js$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/,
+                options: {
+                    presets: ['@babel/preset-env'],
+                }
+            },
+            {//npm install --save-dev css-loader sass-loader sass
+                test: /\.(c|sc|sa)ss$/i,
                 use: [
-                    MiniCssExtractPlugin.loader,
-                    {
-                        loader: 'css-loader',
+                    devMode ? "style-loader" : MiniCssExtractPlugin.loader,
+                    "css-loader",
+                    { // npm install --save-dev postcss-loader postcss postcss-preset-env (Префиксы)
+                        loader: 'postcss-loader',
                         options: {
-                            importLoaders: 1
+                            postcssOptions:  {
+                                plugins: [require('postcss-preset-env')],
+                            }
                         }
                     },
-                    'postcss-loader'
-                ]
+                    "sass-loader",
+                ],
             },
             {
-                test: /\.svg$/,
-                use: 'file-loader'
+                test: /\.(svg|jpg)$/,
+                type: 'asset/resource',
             }
         ]
     },
     plugins: [
         new HtmlWebpackPlugin({
-            templateContent: ({ htmlWebpackPlugin }) => '<!DOCTYPE html><html><head><meta charset=\"utf-8\"><title>' + htmlWebpackPlugin.options.title + '</title></head><body><div id=\"app\"></div></body></html>',
-            filename: 'index.html',
+            template: path.resolve(__dirname, 'src', 'index.html'),
         }),
-        new MiniCssExtractPlugin()
+        new MiniCssExtractPlugin({
+            filename: '[name].[contenthash].css',
+        })
     ]
 };
-
-module.exports = config;
